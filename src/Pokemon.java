270042271aeb6b1d72a50a8aee7550426d961ca7
D0426511 <wchill23@gmail.com>
2017-01-06 15:26:34 +0800

    hw7

---
+++ src/Pokemon.java
@@ -0,0 +1,30 @@
+
+public abstract class Pokemon {
+	
+	private final String name;
+	private final PokemonType type;
+	private int cp;
+
+	Pokemon(String name,PokemonType type,int cp){
+		this.name=name;
+		this.type=type;
+		this.cp=cp;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	public PokemonType getType(){
+		return this.type;
+	}
+	
+	public void setCP(int cp){
+		this.cp=cp;
+	}
+	public int getCP(){
+		return this.cp;
+	}
+	
+	public abstract void attack();
+
+}
